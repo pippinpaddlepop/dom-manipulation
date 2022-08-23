0c9617e3685c08c8511b2020005d14b7841d89d3
Debarshi <impdebarshi@gmail.com>
2022-08-23 19:20:02 +0530

    added the second blue para and the nested div with nested h1 tag

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
+//selecting the container div
 const container = document.querySelector('.container');
+//creating a <p> elemnt 
 const parared = document.createElement('p');
+//adding text and color to the <p> elemnt
 parared.textContent = "hey i am red";
 parared.style.color = 'red';
+//appending the <p> to the container div
 container.appendChild(parared);
 
+//creating an <h3>
+const head3 = document.createElement('h3');
+head3.textContent = 'hey i am blue';
+head3.style.color = 'blue';
+
+//appending to container div
+
+container.appendChild(head3);
+
+
+//creating a nested container div
+const div2 = document.createElement('div');
+div2.style.border = 'black';
+div2.style.backgroundColor  = 'pink';
+
+//appending to container
+
+container.appendChild(div2);
+
+
+//nesting an h1 in div 2
+
+const headdiv = document.createElement('h1');
+headdiv.textContent = "im in a div";
+
+div2.appendChild(headdiv);
