ee32d281e26922691d02bb6224905cd982e8da39
Debarshi <impdebarshi@gmail.com>
2022-08-23 18:55:32 +0530

    added <p> element with text

---
+++ app.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+const parared = document.createElement('p');
+parared.textContent = "hey i am red";
+parared.style.color = 'red';
+container.appendChild(parared);
+
